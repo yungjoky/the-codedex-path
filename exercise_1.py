009c7a6ac8b67f7e4bf8825eb025a660b4a68b4f
yungjoky <tolga.beyti@gmail.com>
2025-02-20 18:01:01 +0200

    Added Files

---
+++ exercise_1.py
@@ -0,0 +1,14 @@
+
+genz_slang = ['no cap', 'fr fr', 'slay', 'sus', 'bussin', 'sheesh', 'based']
+
+print("My Gen Z slang list:")
+print(genz_slang)
+
+print("\nWhat these mean:")
+print(f"'{genz_slang[0]}' - That's the truth!")
+print(f"'{genz_slang[1]}' - For real, for real!")
+print(f"'{genz_slang[2]}' - That's amazing!")
+print(f"'{genz_slang[3]}' - Suspicious")
+print(f"'{genz_slang[4]}' - Really good")
+print(f"'{genz_slang[5]}' - Expression of disbelief")
+print(f"'{genz_slang[6]}' - Cool and agreeable")
